0f83503eb6f901d34a52095164070cf39d6240ee
chaim1 <cohen1ch@gmail.com>
2019-03-28 19:10:36 +0200

    update

--- base-app/src/environments/environment.prod.ts
+++ base-app/src/environments/environment.prod.ts
@@ -1,6 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    
+    apiKey: "AIzaSyCWoaUL-zMPD2T3KvHJK0GWC9vYXvfci48",
+    authDomain: "base-app-a04ec.firebaseapp.com",
+    databaseURL: "https://base-app-a04ec.firebaseio.com",
+    projectId: "base-app-a04ec",
+    storageBucket: "base-app-a04ec.appspot.com",
+    messagingSenderId: "539808614569"
   }
 };
--- base-app/src/environments/environment.ts
+++ base-app/src/environments/environment.ts
@@ -5,7 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-   
+    apiKey: "AIzaSyCWoaUL-zMPD2T3KvHJK0GWC9vYXvfci48",
+    authDomain: "base-app-a04ec.firebaseapp.com",
+    databaseURL: "https://base-app-a04ec.firebaseio.com",
+    projectId: "base-app-a04ec",
+    storageBucket: "base-app-a04ec.appspot.com",
+    messagingSenderId: "539808614569"
   }
 };
 
